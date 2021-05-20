77571466638f4d27501d15b0b008a88316eaca16
Miroslav Hudec <mijujda@gmail.com>
2021-05-21 00:23:34 +0200

    Fix RTD Docs build take 2

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import pathlib
 import sys
-sys.path.insert(0, pathlib.Path(__file__).parent.parent)
+ROOT_DIR = pathlib.Path(__file__).parent.parent.absolute()
+sys.path.insert(0, ROOT_DIR)
 
 
 # -- Project information -----------------------------------------------------
