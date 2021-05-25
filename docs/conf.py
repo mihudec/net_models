0b722ad59b0e49a979c7fda3a043b60d706bbf20
Miroslav Hudec <mijujda@gmail.com>
2021-05-25 15:21:30 +0200

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Miroslav Hudec'
 author = 'Miroslav Hudec'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
