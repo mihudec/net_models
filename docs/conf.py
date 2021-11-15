3040742266185c0d098976be4dce7e89d72c1122
Miroslav Hudec <mijujda@gmail.com>
2021-11-15 14:56:50 +0100

    Bump version: 0.2.0 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Miroslav Hudec'
 author = 'Miroslav Hudec'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import pathlib
 from setuptools import setup, find_packages
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
 def load_requirements():
     requirements = []
